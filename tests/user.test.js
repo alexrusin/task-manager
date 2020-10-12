b83f269a2d73b45b2cd7241a2f577aab06cc3206
Alex Rusin <alexrusin_2000@yahoo.com>
2020-10-12 07:46:25 -0700

    more tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,24 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app');
+const User = require('../src/models/user');
+
+const userOneId = new mongoose.Types.ObjectId();
+const userOne = {
+    _id: userOneId,
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '56what!!',
+    tokens: [{
+        token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+    }]
+}
+
+beforeEach(async () => {
+    await User.deleteMany();
+    await new User(userOne).save();
+});
 
 test('Should sign up a new user', async () => {
     await request(app).post('/users').send({
@@ -8,3 +27,48 @@ test('Should sign up a new user', async () => {
         password: 'MyPass777!'
     }).expect(201)
 });
+
+test('Should log in existing user', async() => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200);
+})
+
+test('Should should not login user if credentials are invalid', async() => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: 'thisIsNotMyPassword'
+    }).expect(400);
+})
+
+test('Should get profile for user', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200);
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401);
+})
+
+// sends an email.  Address issue later
+// test('Should delete profile for user', async () => {
+//     await request(app)
+//         .delete('/users/me')
+//         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+//         .send()
+//         .expect(200);
+// })
+
+test('Should not delete profile for user if not authenticated', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401);
+})
