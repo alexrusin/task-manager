83a5a2dc43f8bdffbf7517beaff278d6bacec90b
Alex Rusin <alexrusin_2000@yahoo.com>
2020-10-15 07:52:35 -0700

    add sorting and pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,18 +18,32 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
 
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1; // asc: 1; desc: -1 
     }
 
     try {
         await req.user.populate({
             path: 'tasks',
             match,
+            options: {
+                limit: parseInt(req.query.limit) // if limit not provided or NaN it will be ignored
+                skip: parseInt(req.query.skip),
+                sort
+            }
         }).execPopulate()
         res.send(req.user.tasks);
     } catch (e) {
